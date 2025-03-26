cbecdc06a7d473a83d677376bdf328a561caf5ab
ayushchauhan7 <chauhan0007ayush@gmail.com>
2025-03-26 19:08:00 +0530

    second

--- js/main.js
+++ js/main.js
@@ -16,8 +16,33 @@ const initApp = () => {
     processSubmission();
   });
 
+  const clearItems = document.getElementById("clearItems");
+  clearItems.addEventListener("click", (event) => {
+    const list = toDoList.getList();
+    if (list.length) {
+      const confirmed = confirm("Are you sure you want to clear the entire list?");
+      if (confirmed) {
+        toDoList.clearList();
+    updatePersistentData(toDoList.getList());
+        refreshThePage();
+      }
+    }
+  })
+
+  loadListObject();
   refreshThePage();
 };
+
+const loadListObject = () => {
+  const storedList = localStorage.getItem("myToDoList");
+  if (typeof storedList !== "string") return;
+  const parsedList = JSON.parse(storedList);
+  parsedList.forEach(itemObj => {
+    const newToDoItem = createNewItem(itemObj._id, itemObj._item);
+    toDoList.addItemToList(newToDoItem);
+  })
+}
+
 
 const refreshThePage = () => {
   clearListDisplay();
@@ -49,7 +74,7 @@ const renderList = () => {
 
 const buildListItem = (item) => {
   const div = document.createElement("div");
-  div.ClassName = "item";
+  div.className = "item";
   const check = document.createElement("input");
   check.type = "checkbox";
   check.id = item.getId();
@@ -67,10 +92,21 @@ const buildListItem = (item) => {
 const addClickListenerToCheckBox = (checkbox) => {
   checkbox.addEventListener("click", (event) => {
     toDoList.removeItemFromList(checkbox.id);
+    updatePersistentData(toDoList.getList());
+    const removedText = getLabelText(checkbox.id);
+    updateScreenReaderConfirmation(removedText, "removed from list");
     setTimeout(() => {
       refreshThePage();
     }, 1000);
   });
+};
+
+const getLabelText = (checkboxId) => { 
+  return document.getElementById(checkboxId).nextElementSibling.textContent;
+}
+
+const updatePersistentData = (listArray) => {
+  localStorage.setItem("myToDoList", JSON.stringify(listArray));
 };
 
 const clearItemEntryField = () => {
@@ -83,10 +119,12 @@ const setFocusOnItemEntry = () => {
 
 const processSubmission = () => {
   const newEntryText = getNewEntry();
-  if (newEntryText.length) return;
+  if (!newEntryText.length) return;
   const nextItemId = calcNextItemId();
   const toDoItem = createNewItem(nextItemId, newEntryText);
   toDoList.addItemToList(toDoItem);
+  updatePersistentData(toDoList.getList());
+  updateScreenReaderConfirmation(newEntryText, "added");
   refreshThePage();
 };
 
@@ -108,4 +146,8 @@ const createNewItem = (itemId, itemText) => {
   toDo.setId(itemId);
   toDo.setItem(itemText);
   return toDo;
+};
+
+const updateScreenReaderConfirmation = (newEntryText, actionVerb) => {
+  document.getElementById("confirmation").textContent = `${newEntryText} ${actionVerb}.`;
 }
